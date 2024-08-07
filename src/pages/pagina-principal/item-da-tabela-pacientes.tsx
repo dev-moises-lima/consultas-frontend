df2eac3882f90169ac0f12dd54131b44720abf21
Pedro Luan <macielpedro794@gmail.com>
2024-08-07 12:39:50 -0300

    Adicionando estilização a cor da condição do paciente

--- src/pages/pagina-principal/item-da-tabela-pacientes.tsx
+++ src/pages/pagina-principal/item-da-tabela-pacientes.tsx
@@ -2,6 +2,7 @@ import moment from "moment";
 import { Paciente } from "../../lib/minhas-interfaces-e-tipos";
 import { Button } from "react-bootstrap";
 import { FaArrowRight } from "react-icons/fa";
+import { obterCorDaCondicao } from "../../lib/minhas-funcoes";
 
 interface ItemDaTabelaPacientesProps {
   paciente: Paciente
@@ -13,12 +14,13 @@ export function ItemDaTabelaPacientes({
   const hoje = moment()
   const dataDeNascimento = moment(paciente.data_de_nascimento, "YYYY/MM/DD")
   const idadeDoPaciente = hoje.diff(dataDeNascimento, "year")
+  const corDaCondicao = obterCorDaCondicao(paciente.condicao_atual)
 
   return (
     <tr>
       <td className="align-middle">{paciente.id}</td>
       <td className="align-middle">{paciente.nome}</td>
-      <td className="align-middle">
+      <td className={`align-middle text-${corDaCondicao}`}>
         {paciente.condicao_atual || "Nenhuma consulta foi feita"}
       </td>
       <td className="align-middle">{paciente.cpf}</td>
